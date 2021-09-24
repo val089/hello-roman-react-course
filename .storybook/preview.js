697d5021ea6be50fd0db96dd2a8567d7fbd57100
Kamil Szerląg <kamilszerlag@gmail.com>
2021-09-24 17:33:21 +0200

    storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,23 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'assets/styles/theme';
+import { GlobalStyle } from 'assets/styles/GlobalStyle';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Story />
+    </ThemeProvider>
+  ),
+];
